9d0eab071537414af04bf8b08a365ac78812a183
Wagram Airian <vagram.airiian@tum.de>
2017-07-09 12:17:15 +0200

    Rollback

--- user/userController.js
+++ user/userController.js
@@ -1,58 +1,63 @@
 var Config = require('../config/config.js');
 var User = require('./userSchema');
 var jwt = require('jwt-simple');
-var express = require('express');
-var userService = require('./userService');
 
 module.exports.login = function(req, res){
-    userService.authenticate(req.body.email, req.body.password)
-        .then(function (user) {
-            if (user) {
-                // authentication successful
-                res.send(user);
+
+    if(!req.body.email){
+        res.status(400).send('email required');
+        return;
+    }
+    if(!req.body.password){
+        res.status(400).send('password required');
+        return;
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if (err) {
+            res.status(500).send(err);
+            return
+        }
+
+        if (!user) {
+            res.status(401).send('Invalid Credentials');
+            return;
+        }
+        user.comparePassword(req.body.password, function(err, isMatch) {
+            if(!isMatch || err){
+                res.status(401).send('Invalid Credentials');
             } else {
-                // authentication failed
-                res.status(400).send('Username or password is incorrect');
+                res.status(200).json({token: createToken(user)});
             }
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
         });
+    });
+
 };
 
 module.exports.signup = function(req, res){
-    userService.create(req.body)
-        .then(function () {
-            res.sendStatus(200);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
+    if(!req.body.email){
+        res.status(400).send('email required');
+        return;
+    }
+    if(!req.body.password){
+        res.status(400).send('password required');
+        return;
+    }
+
+    var user = new User();
+
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save(function(err) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.status(201).json({token: createToken(user)});
+    });
 };
-
-module.exports.getCurrent = function(req, res) {
-    userService.getById(req.user.sub)
-        .then(function (user) {
-            if (user) {
-                res.send(user);
-            } else {
-                res.sendStatus(404);
-            }
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
-}
-
-module.exports.update = function(req, res) {
-    userService.update(req.params._id, req.body)
-        .then(function () {
-            res.sendStatus(200);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
-}
 
 module.exports.unregister = function(req, res) {
     req.user.remove().then(function (user) {
@@ -66,7 +71,7 @@ function createToken(user) {
     var tokenPayload = {
         user: {
             _id: user._id,
-            username: user.username
+            email: user.email
         }
 
     };
--- user/userRoutes.js
+++ user/userRoutes.js
@@ -10,8 +10,6 @@ function userRoutes(passport) {
     router.post('/login', userController.login);
     router.post('/signup', userController.signup);
     router.post('/unregister', passport.authenticate('jwt', {session: false}),userController.unregister)
-	router.get('/current', userController.getCurrent);
-	router.put('/:_id', userController.update);
 
     return router;
 
